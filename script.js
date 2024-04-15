9c895192efb0b4260a1a037490e5ddaeaa4c25e1
bammer25 <charliejunk38@gmail.com>
2024-04-15 07:52:22 -0500

    FootballClub

---
+++ script.js
@@ -0,0 +1,83 @@
+// Get references to HTML elements
+const clubList = document.getElementById('club-list');
+const searchInput = document.getElementById('search');
+const clubDetailsContainer = document.getElementById('main');
+
+// Attach an input event listener for the search input
+searchInput.addEventListener('input', handleSearchInput);
+
+// Initialize football club data and display all clubs
+let clubData = footballClubs; 
+displayClubs(footballClubs);
+
+
+// Display football clubs in the club list
+function displayClubs(clubs) {
+    // Generate HTML for club cards and set it in the clubList element
+    const clubCardsHTML = clubs.map(createClubCardHTML).join('');
+    clubList.innerHTML = clubCardsHTML;
+}
+
+// Create HTML for a football club card
+function createClubCardHTML(club) {
+    return `
+        <div class="club-card" onclick="handleClubClick(this);"><!-- Add onclick event -->
+            <h2>${club.name}</h2>
+            <img src="${club.image}" alt="${club.name} Image" style="width:100%; height:20vh;">
+            <p><b>League: </b>${club.league}</p>
+            <p><b>City: </b>${club.city}</p>
+            <button onclick="viewClubPlayers('${club.name}'); event.stopPropagation();" style="width:100%;">View Players</button>
+        </div>
+    `;
+}
+
+// Handle clicking on a football club card
+function handleClubClick(element) {
+    // Write your code here for task1
+        // Get the name of the clicked club
+        
+        // Find the selected club by its name
+              
+        // Display details of the selected club
+            
+    }
+
+
+// Display football club details
+function displayClubDetails(club) {
+    // Write your code here for task2
+        // Create a club details HTML using template strings
+    
+    // Set the club details HTML in the clubDetailsContainer
+    
+}
+
+// Function to view club players
+function viewClubPlayers(clubName) {
+    // Find the selected club by its name
+    const selectedClub = clubData.find(club => club.name === clubName);
+    
+    // Write your code here for task3
+    // Generate HTML for the list of players and display it
+
+    // Iterate over selectedClub object's players property
+
+    // Create a string joining the information of all the players of the selected Club 
+
+    // Display the information by setting the HTML in the clubDetailsContainer
+    
+}
+
+// Handle search input and filter clubs
+function handleSearchInput() {
+    // Write your code here for task4
+
+    // Get the search term and convert it to lowercase for case-insensitive search
+    
+    // Create a string containing club details for searching
+    
+    // Check if the search term is found in the club data string
+        
+    // Display the filtered clubs
+    
+}
