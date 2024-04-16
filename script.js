25fe69b810b8d341e47f03388b4cb76685a23ab7
bammer25 <charliejunk38@gmail.com>
2024-04-16 08:04:18 -0500

    change

--- script.js
+++ script.js
@@ -35,10 +35,17 @@ function createClubCardHTML(club) {
 function handleClubClick(element) {
     // Write your code here for task1
         // Get the name of the clicked club
-        
+        let clubName = element.getElementsByTagName("h2")[0].innerHTML;
+
+        console.log(clubName);
+
         // Find the selected club by its name
-              
+        let clubCard =clubData.find(obj => obj.name === clubName);
+        
+        console.log(clubCard);
+
         // Display details of the selected club
+        displayClubDetails(clubCard);
             
     }
 
@@ -47,18 +54,36 @@ function handleClubClick(element) {
 function displayClubDetails(club) {
     // Write your code here for task2
         // Create a club details HTML using template strings
+        const clubDetailsHTML = `
+        <div class="club-card" onclick="handleClubClick(this);"><!-- Add onclick event -->
+            <h2>${club.name}</h2>
+            <img src="${club.image}" alt="${club.name} Image" style="width:100%; height:20vh;">
+            <p><b>League: </b>${club.league}</p>
+            <p><b>City: </b>${club.city}</p>
+            <button onclick="viewClubPlayers('${club.name}'); event.stopPropagation();" style="width:100%;">View Players</button>
+        </div>
+    
+        `;
+
+        console.log(clubDetailsHTML);
     
     // Set the club details HTML in the clubDetailsContainer
+    clubDetailsContainer.innerHTML = clubDetailsHTML;
     
 }
 
 // Function to view club players
 function viewClubPlayers(clubName) {
     // Find the selected club by its name
-    const selectedClub = clubData.find(club => club.name === clubName);
+    let clubPlayer = document.getElementById("club-list");
+
+    console.log(clubName);
+
     
     // Write your code here for task3
     // Generate HTML for the list of players and display it
+    let teamPlaters = document.getElementById(players);
+    
 
     // Iterate over selectedClub object's players property
 
